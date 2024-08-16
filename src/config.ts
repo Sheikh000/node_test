2d10dfe836cd760c15ca22d51bb093d135e7e4a4
safin <safnfiroz.sheikh@keit.io>
2024-08-16 15:53:26 +0530

    feat:Intialize Config file

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+/**Config.ts */
+interface ConfigType {
+	server: {
+		port: number | string;
+	};
+}
+
+const Config: ConfigType = {
+	server: {
+		port: process.env.PORT || 3000,
+	},
+};
+export default Config
