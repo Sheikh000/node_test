c9143c19ac1e97a1673ede7babb96eb64e2c9fb3
Sheikh S <isafinsheikh@gmail.com>
2024-08-27 14:19:06 +0530

    update: batch schema

--- src/components/batch/batch.model.ts
+++ src/components/batch/batch.model.ts
@@ -1,33 +1,31 @@
 /**user.model.ts */
 import { Schema, model, Document } from 'mongoose';
 import { DEPARTMENT } from '../user/user.enum';
+interface IBranch {
+	name: string;
+	totalStudentsIntake: number;
+}
 interface IBatch extends Document {
 	year: number;
-	branches: [
-		{
-			name: DEPARTMENT;
-			totalStudentsIntake: number;
-		},
-	];
+	branches: IBranch[];
 }
+const branchSchema = new Schema<IBranch>({
+	name: {
+		type: String,
+		required: true,
+		enum: DEPARTMENT,
+	},
+	totalStudentsIntake: {
+		type: Number,
+		required: true,
+	},
+});
 const batchSchema = new Schema<IBatch>({
 	year: {
 		type: Number,
 		required: true,
 	},
-	branches: [
-		{
-			name: {
-				type: String,
-				enum: DEPARTMENT,
-				required: true,
-			},
-			totalStudentsIntake: {
-				type: Number,
-				required: true,
-			},
-		},
-	],
+	branches: [branchSchema],
 });
 
 const Batch = model<IBatch>('Batches', batchSchema);
